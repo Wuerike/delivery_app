34ceac562403cec3b5679cdcd7854ad07bf8f524
undefined <undefined@noreply>
2022-12-31 01:22:14 -0300

    chore: Add routing

---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+// Pages
+import 'package:delivery_app/src/pages/welcome_page.dart';
+
+final routes = <String, WidgetBuilder>{
+  'welcome': (BuildContext context) => WelcomePage()
+};
