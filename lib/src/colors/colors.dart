b2ee6442be5e002e5339284056eda6c232ef8b70
undefined <undefined@noreply>
2022-12-31 01:21:15 -0300

    chore: Add colors

---
+++ lib/src/colors/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+// Primary
+const accentColor = Color.fromRGBO(10, 31, 68, 1.0);
+const yellow = Color.fromRGBO(255, 204, 0, 1.0);
+const greyColor = Color.fromRGBO(184, 187, 198, 1.0);
+const pink = Color.fromRGBO(255, 45, 85, 1.0);
+const white = Colors.white;
+const black = Colors.black;
+const orange = Color.fromRGBO(255, 140, 0, 1.0);
+
+// Customs
+const orangeWithHalfOpacity = Color.fromRGBO(255, 140, 0, 0.5);
+
+// Components
+const fbButtonColor = Color.fromRGBO(0, 122, 255, 1.0);
+
+// Backgrounds
+const bgGreyPage = Color.fromRGBO(247, 248, 250, 1.0);
+const bgInputs = Color.fromRGBO(142, 142, 147, 1.2);
