f83bff1eaf46f59c412497d1eda86f14fc53afbb
undefined <undefined@noreply>
2023-01-02 16:08:12 -0300

    chore: Add dummy tabs

---
+++ lib/src/tabs/explore_tab.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ExploreTab extends StatelessWidget {
+  const ExploreTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      child: Text("Explore"),
+    ));
+  }
+}
---
+++ lib/src/tabs/my_order_tab.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyOrderTab extends StatefulWidget {
+  MyOrderTab({Key? key}) : super(key: key);
+
+  @override
+  State<MyOrderTab> createState() => _MyOrderTabState();
+}
+
+class _MyOrderTabState extends State<MyOrderTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      child: Text("My Order"),
+    ));
+  }
+}
