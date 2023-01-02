06216e9f47a2ef921f7d30da84d1413d00fe3511
undefined <undefined@noreply>
2023-01-01 23:59:16 -0300

    feat: Add sign up page

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:delivery_app/src/pages/forgot_password_page.dart';
 import 'package:delivery_app/src/pages/login_page.dart';
+import 'package:delivery_app/src/pages/sign_up_page.dart';
 import 'package:flutter/widgets.dart';
 
 // Pages
@@ -8,5 +9,6 @@ import 'package:delivery_app/src/pages/welcome_page.dart';
 final routes = <String, WidgetBuilder>{
   'welcome': (BuildContext context) => WelcomePage(),
   'login': (BuildContext context) => LoginPage(),
-  'forgot-password': (BuildContext context) => ForgotPasswordPage()
+  'forgot-password': (BuildContext context) => ForgotPasswordPage(),
+  'sign-up': (BuildContext context) => SingUpPage()
 };
