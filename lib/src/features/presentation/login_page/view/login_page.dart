49d8d041aa86da04ee9b48e59b2f9b50cc6c2a97
undefined <undefined@noreply>
2023-01-04 20:06:50 -0300

    fix: Define back button as component

--- lib/src/features/presentation/login_page/view/login_page.dart
+++ lib/src/features/presentation/login_page/view/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:delivery_app/src/colors/colors.dart';
-import 'package:delivery_app/src/features/presentation/widgets/back_button.dart';
+import 'package:delivery_app/src/features/presentation/shared/components/buttons/back_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
