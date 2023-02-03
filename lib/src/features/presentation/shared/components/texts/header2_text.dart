2a0499b6895cdb27b62637438a287c86fc7b2fb8
undefined <undefined@noreply>
2023-02-03 17:30:02 -0300

    feat: Add profile page

--- lib/src/features/presentation/shared/components/texts/header2_text.dart
+++ lib/src/features/presentation/shared/components/texts/header2_text.dart
@@ -8,6 +8,7 @@ Widget header2Text(
   Color color = Colors.black,
   Color? strokeColor,
   TextAlign textAlign = TextAlign.start,
+  FontWeight fontWeight = FontWeight.bold,
   TextOverflow? overflow,
 }) {
   return Text(
@@ -16,7 +17,7 @@ Widget header2Text(
     overflow: overflow,
     style: TextStyle(
       color: color,
-      fontWeight: FontWeight.bold,
+      fontWeight: fontWeight,
       fontSize: fontSize ?? 20.sp,
       shadows: strokeColor == null ? [] : outlinedTextShadow(strokeColor: strokeColor),
     ),
