529a78267b0b2bad1e765eb4a6110cb9d21247dd
undefined <undefined@noreply>
2023-02-05 09:55:18 -0300

    chore: Add interfaces

---
+++ lib/src/services/firebase_services/auth_service/interfaces/interfaces.dart
@@ -0,0 +1,20 @@
+import 'package:delivery_app/src/base/api_service/api_service.dart';
+
+abstract class BaseAuthFirebaseService {
+  ApiServiceAbstraction apiService = ApiService();
+  static String baseUrl = "https://identitytoolkit.googleapis.com/v1/";
+  //TODO: Please change this admin token for yours. ;)
+  static String adminToken = "AIzaSyCaA1EYAlqoylaDRaaw3YU0lSuxYO7jRQk";
+  static String signUpEndpoint = "accounts:signUp?key=$adminToken";
+  static String signInEndpoint = "accounts:signInWithPassword?key=$adminToken";
+}
+
+abstract class SignUpServiceAbstraction extends BaseAuthFirebaseService {
+  String endpoint = BaseAuthFirebaseService.baseUrl + BaseAuthFirebaseService.signUpEndpoint;
+  Future<Map<String, dynamic>> signUp({required Map<String, dynamic> body});
+}
+
+abstract class SignInServiceAbstraction extends BaseAuthFirebaseService {
+  String endpoint = BaseAuthFirebaseService.baseUrl + BaseAuthFirebaseService.signInEndpoint;
+  Future<Map<String, dynamic>> signIn({required Map<String, dynamic> body});
+}
