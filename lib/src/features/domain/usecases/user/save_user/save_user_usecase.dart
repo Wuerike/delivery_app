b09b0fe1ca29ddcf0aaa12a9cfcb962be4b85731
Wuerike <wuerike.henri@gmail.com>
2023-02-09 08:27:57 -0300

    feat: Add save user usecase

---
+++ lib/src/features/domain/usecases/user/save_user/save_user_usecase.dart
@@ -0,0 +1,28 @@
+import 'package:delivery_app/src/base/app_error/app_error.dart';
+import 'package:delivery_app/src/features/data/interfaces/interfaces.dart';
+import 'package:delivery_app/src/features/domain/entities/user/user_entity.dart';
+import 'package:delivery_app/src/utils/result_type/result_type.dart';
+
+abstract class SaverUserUsecaseAbstraction {
+  Future<Result<UserEntity, Failure>> execute({required UserEntity user});
+}
+
+class SaverUserUsecase extends SaverUserUsecaseAbstraction {
+  // * Dependencies
+  final SaveUserRepositoryAbstraction _repository;
+
+  SaverUserUsecase({required SaveUserRepositoryAbstraction repository}) : _repository = repository;
+
+  @override
+  Future<Result<UserEntity, Failure>> execute({required UserEntity user}) {
+    return _repository.saveUser(user: user).then((result) {
+      switch (result.status) {
+        case ResultStatus.success:
+          var entity = UserEntity.fromMap(result.value!.toMap());
+          return Result.success(entity);
+        case ResultStatus.error:
+          return Result.failure(result.error);
+      }
+    });
+  }
+}
