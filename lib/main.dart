e825e87993e0c24c85fa9b9e5a2780fa81a9c20f
Wuerike <wuerike.henri@gmail.com>
2023-02-09 08:11:00 -0300

    feat: Add firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:delivery_app/firebase_options.dart';
 import 'package:delivery_app/src/colors/colors.dart';
 import 'package:delivery_app/src/routes/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
