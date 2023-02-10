d05382a3a384bada4b2463b9e39ceb0447831837
Wuerike <wuerike.henri@gmail.com>
2023-02-10 10:46:34 -0300

    chore: Add update password service

--- lib/src/services/firebase_services/auth_service/interfaces/interfaces.dart
+++ lib/src/services/firebase_services/auth_service/interfaces/interfaces.dart
@@ -3,10 +3,10 @@ import 'package:delivery_app/src/base/api_service/api_service.dart';
 abstract class BaseAuthFirebaseService {
   ApiServiceAbstraction apiService = ApiService();
   static String baseUrl = "https://identitytoolkit.googleapis.com/v1/";
-  //TODO: Please change this admin token for yours. ;)
   static String adminToken = "AIzaSyCaA1EYAlqoylaDRaaw3YU0lSuxYO7jRQk";
   static String signUpEndpoint = "accounts:signUp?key=$adminToken";
   static String signInEndpoint = "accounts:signInWithPassword?key=$adminToken";
+  static String updatePasswordEndpoint = "accounts:sendOobCode?key=$adminToken";
 }
 
 abstract class SignUpServiceAbstraction extends BaseAuthFirebaseService {
@@ -17,4 +17,9 @@ abstract class SignUpServiceAbstraction extends BaseAuthFirebaseService {
 abstract class SignInServiceAbstraction extends BaseAuthFirebaseService {
   String endpoint = BaseAuthFirebaseService.baseUrl + BaseAuthFirebaseService.signInEndpoint;
   Future<Map<String, dynamic>> signIn({required Map<String, dynamic> body});
+}
+
+abstract class UpdatePasswordServiceAbstraction extends BaseAuthFirebaseService {
+  String endpoint = BaseAuthFirebaseService.baseUrl + BaseAuthFirebaseService.updatePasswordEndpoint;
+  Future<Map<String, dynamic>> updatePassword({required String email});
 }
