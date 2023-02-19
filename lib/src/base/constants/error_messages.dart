956c498e353b71b7a5fbed184c7aec2851fbabaf
Wuerike <wuerike.henri@gmail.com>
2023-02-18 23:24:56 -0300

    chore: Translate error messages to english

--- lib/src/base/constants/error_messages.dart
+++ lib/src/base/constants/error_messages.dart
@@ -6,11 +6,10 @@ abstract class FirebaseFailureMessages {
   static String emailNotFound =
       "There is no user record corresponding to this identifier. The user may have been deleted.";
   static String invalidPassword = "The password is invalid or the user does not have a password.";
-  static String emailExist = "La dirección de correo electrónico ya está siendo utilizada por otra cuenta.";
+  static String emailExist = "The email address is already in use by another account.";
   static String tooManyAttempts =
-      "Hemos bloqueado todas las solicitudes de este dispositivo debido a una actividad inusual. Vuelve a intentarlo más tarde.";
-  static String invalidIdToken =
-      "La credencial del usuario ya no es válida. El usuario debe iniciar sesión nuevamente.";
+      "We have blocked all requests for this device due to unusual activity. Try again later.";
+  static String invalidIdToken = "The user's credential is no longer valid. The user must log in again.";
   static String userNotFound =
-      "No hay ningún registro de usuario que corresponda a este identificador. Es posible que se haya eliminado al usuario.";
+      "There is no user record corresponding to this identifier. The user may have been removed.";
 }
