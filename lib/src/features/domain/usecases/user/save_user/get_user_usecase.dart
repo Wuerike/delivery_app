5858864dbb424fae64e103ac6a14e97fc0e3356b
Wuerike <wuerike.henri@gmail.com>
2023-07-17 18:19:04 -0300

    chore: Simplify get user usecase code

--- lib/src/features/domain/usecases/user/save_user/get_user_usecase.dart
+++ lib/src/features/domain/usecases/user/save_user/get_user_usecase.dart
@@ -1,5 +1,6 @@
 import 'package:delivery_app/src/base/app_error/app_error.dart';
 import 'package:delivery_app/src/features/data/interfaces/interfaces.dart';
+import 'package:delivery_app/src/features/data/repositories/user/get_user_repository.dart';
 import 'package:delivery_app/src/features/domain/entities/user/user_entity.dart';
 import 'package:delivery_app/src/utils/result_type/result_type.dart';
 
@@ -9,20 +10,12 @@ abstract class GetUserUsecaseAbstraction {
 
 class GetUserUsecase extends GetUserUsecaseAbstraction {
   // * Dependencies
-  final GetUserRepositoryAbstraction _repository;
+  final GetUserRepositoryAbstraction repository;
 
-  GetUserUsecase({required GetUserRepositoryAbstraction repository}) : _repository = repository;
+  GetUserUsecase({GetUserRepositoryAbstraction? repository}) : repository = repository ?? GetUserRepository();
 
   @override
-  Future<Result<UserEntity, Failure>> execute({required String userId}) {
-    return _repository.getUser(userId: userId).then((result) {
-      switch (result.status) {
-        case ResultStatus.success:
-          var entity = UserEntity.fromMap(result.value!.toMap());
-          return Result.success(entity);
-        case ResultStatus.error:
-          return Result.failure(result.error);
-      }
-    });
+  Future<Result<UserEntity, Failure>> execute({required String userId}) async {
+    return await repository.getUser(userId: userId);
   }
 }
